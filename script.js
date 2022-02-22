b2b84cd58f45717da9d27b1a94f61a3ce6833fd4
SamuelScratch <kevinsamalex@gmail.com>
2022-02-22 10:42:11 +0100

    starting project

---
+++ script.js
@@ -0,0 +1,24 @@
+var navbar = `
+<nav id="navbar">
+    <div id="icon">
+    </div>
+    <div id="menu">
+        <div>
+            <a href="./index.html" id="main-item"><span>Accueil</span></a>
+        </div>
+        <div>
+            <a href="./presentation.html"><span>Qui suis-je ?</span></a>
+        </div>
+        <div>
+            <a href="./prestations.html"><span>Prestations</span></a>
+        </div>
+        <div>
+            <a href="./tarifs.html"><span>Tarifs</span></a>
+        </div>
+        <div>
+            <a href="./contact.html"><span>Contact</span></a>
+        </div>
+    </div>
+</nav>
+`
+document.write(navbar)
